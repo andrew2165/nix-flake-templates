82b96e0c0f3c465b11e1ead2c8156f964dafc5c1
Jonathan Stewart <jonathanstewart@Jonathans-MacBook-Pro.local>
2025-10-26 21:03:17 -0700

    tried fixing error

--- flake.nix
+++ flake.nix
@@ -3,15 +3,18 @@
     Please look inside the proper language folder. This file is only here, so
     the template option of `nix flake new` works!
   '';
-  outputs =
-    { self }:
-    {
+
+  outputs = { self, ... }:
+    let
       templates = {
         rlang = {
           path = ./rlang;
           description = "A simple starter rlang analysis template with RStudio";
         };
       };
-      defaultTemplate = self.templates.rlang;
+    in {
+      templates = templates;
+      defaultTemplate = templates.rlang;
     };
 }
+
