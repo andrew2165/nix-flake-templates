f4efbec8fbbf37464bca668478f9dd90648ec5a8
Jonathan Stewart <jonathanstewart@Jonathans-MacBook-Pro.local>
2025-10-26 20:54:58 -0700

    ran formatter

--- flake.nix
+++ flake.nix
@@ -1,15 +1,17 @@
 {
   description = ''
-  Please look inside the proper language folder. This file is only here, so
-  the template option of `nix flake new` works!
+    Please look inside the proper language folder. This file is only here, so
+    the template option of `nix flake new` works!
   '';
-  outputs = {self}: {
-    templates = {
-      rlang = {
-        path = ./rlang;
-        description = "A simple starter rlang analysis template with RStudio";
+  outputs =
+    { self }:
+    {
+      templates = {
+        rlang = {
+          path = ./rlang;
+          description = "A simple starter rlang analysis template with RStudio";
+        };
       };
+      defaultTemplate = self.templates.rlang;
     };
-    defaultTemplate = self.templates.rlang;
-  };
 }
