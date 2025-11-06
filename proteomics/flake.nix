3eb63c727827017ed551cae768bbae2a94c0214c
Jonathan Stewart <35748678+andrew2165@users.noreply.github.com>
2025-11-05 20:57:21 -0800

    Refactor comments and update FragPipe fetch method
    Updated comments for clarity and changed fetch method.

--- proteomics/flake.nix
+++ proteomics/flake.nix
@@ -18,28 +18,20 @@
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
       # FragPipe Version
-      # (assumes they keep versioning and naming consistent)
+      # (assumes they keep url versioning and naming consistent)
       version = "23.1";
     in
     {
       devShells = forAllSystems (
         system:
         let
-          #."${system}".default = let
+          
           pkgs = import nixpkgs {
             inherit system;
           };
-          # fragpipeSrc = pkgs.fetchFromGitHub {
-          #   owner = "Nesvilab";
-          #   repo = "FragPipe";
-          #   rev = "d8bf745";
-          #   sha256 = "0000000000000000000000000000000000000000000000000000";
-          # };
+
           fragpipeSrc = pkgs.fetchzip {
-            # URL pattern for the release asset:
-            # https://github.com/Nesvilab/FragPipe/releases/download/23.1/FragPipe-23.1-linux.zip
             url = "https://github.com/Nesvilab/FragPipe/releases/download/${version}/FragPipe-${version}-linux.zip";
-            # Put the correct hash here (see instructions below to prefetch).
             sha256 = "sha256-IIG8WvcpUCChpHZwVDbiMkwWIFrbElt4fagOBeLU560=";
           };
 
