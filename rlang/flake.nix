8d93e2cbb01d54eb9964899e17f82e13a3e62d12
Jonathan Stewart <jonathanstewart@Jonathans-MacBook-Pro-2.local>
2025-11-08 20:57:56 -0800

    updated rlang flake to allow multiple system architectures

--- rlang/flake.nix
+++ rlang/flake.nix
@@ -3,37 +3,40 @@
   # use: nix develop
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
   };
 
-  outputs =
+   outputs =
     { self, nixpkgs, ... }:
     let
-      # system should match the system you are running on
-      # system = "x86_64-linux";
-      system = "aarch64-darwin";
+      supportedSystems = [
+        "aarch64-darwin"
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
     in
     {
-      devShells."${system}".default =
+      devShells = forAllSystems (
+        system:
         let
-          pkgs = import nixpkgs {
-            inherit system;
-          };
+          pkgs = import nixpkgs { inherit system; };
         in
-        pkgs.mkShell {
-          # create an environment with required R Packages
-          packages = with pkgs; [
-            (rWrapper.override { packages = [ rPackages.rstudio_prefs ]; })
-            curlFull
-            rPackages.curl
-            (rstudioWrapper.override {
-              packages = with rPackages; [
-                tidyverse
-                drc
-                rstudio_prefs
-              ]; # add new R packages (from nix) here to get tied in
-            })
-          ];
+        {
+          default = pkgs.mkShell {
+            # create an environment with required R Packages
+            packages = with pkgs; [
+              (rWrapper.override { packages = [ rPackages.rstudio_prefs ]; })
+              curlFull
+              rPackages.curl
+              (rstudioWrapper.override {
+                packages = with rPackages; [
+                  tidyverse
+                  drc
+                  rstudio_prefs
+                ]; # add new R packages (from nix) here to get tied in
+              })
+            ];
 
           shellHook = ''
             	      R -e "require(rstudio.prefs); rstudio_config_path('./rstudio-prefs.json');"
@@ -41,6 +44,7 @@
             	      rstudio
           '';
         };
+    });
 
       # Also output rstudioPrefs for use in other flakes
       rstudioPrefs = ./rstudio-prefs.json;
